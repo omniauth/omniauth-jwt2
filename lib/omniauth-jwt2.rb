ca0fecd83a4b8ad41db50647ce8e55f06952f4a4
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 09:05:04 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- lib/omniauth-jwt2.rb
+++ lib/omniauth-jwt2.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
 require "omniauth/jwt2"
+require "version_gem"
+require_relative "omniauth/jwt2/version"
+
+Omniauth::JWT2::Version.class_eval do
+  extend VersionGem::Basic
+end
--- lib/omniauth/jwt2.rb
+++ lib/omniauth/jwt2.rb
@@ -1,10 +1,3 @@
 # frozen_string_literal: true
 
-require "version_gem"
-
 require_relative "jwt"
-require_relative "jwt2/version"
-
-Omniauth::JWT2::Version.class_eval do
-  extend VersionGem::Basic
-end
